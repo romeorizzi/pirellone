86551cd7a1feffdc1b2b27cb9e0936f82e803bf7
Romeo Rizzi <romeo.rizzi@univr.it>
2019-03-05 21:22:57 +0100

    aggiunte soluzione di test

---
+++ solutions/sol_rilegge_cella.cpp
@@ -0,0 +1,30 @@
+int is_solvable(int m, int n, int is_on(int row, int col)) {
+  int firstRow[n+1];
+  int invFirstRow[n+1];
+  for(int j=1; j<=n; j++) {
+    firstRow[j] = is_on(1, j);
+    invFirstRow[j] = 1-firstRow[j];
+  }
+  for(int i=2; i<=m; i++)
+    if(is_on(i, 1) == firstRow[1]) {
+      for(int j=2; j<=n; j++) {
+	if(is_on(i, j) != firstRow[j])
+	  return 0;
+      }
+    }  
+    else {
+      for(int j=2; j<=n; j++) {
+	if(is_on(i, j) != invFirstRow[j])
+	  return 0;
+      }
+    }
+  return 1;  
+}
+void solve(int n, int m, int is_on(int row, int col), void switch_row(int row), void switch_col(int col)) {
+  for(int j=1; j<=n; j++)
+    if(is_on(1, j))
+      switch_col(j);
+  for(int i=1; i<=m; i++)
+    if(is_on(i, 1))
+      switch_row(i);
+}
